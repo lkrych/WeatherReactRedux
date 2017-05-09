59013fc3377809ca7e373077b35792ed15b29cb3
Leland Krych <Leland.Krych@gmail.com>
2017-05-08 20:13:45 -0700

    Add weather reducer

--- WeatherRedux/src/containers/search_bar.js
+++ WeatherRedux/src/containers/search_bar.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchWeather }  from '../actions/index'
+import { fetchWeather }  from '../actions/index';
 
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {term: ''};
         this.onInputChange = this.onInputChange.bind(this);
         this.onFormSubmit = this.onFormSubmit.bind(this);
     }
-    
+
     onInputChange(event) {
         this.setState( {term: event.target.value });
     }
-    
+
     onFormSubmit(event) {
         event.preventDefault();
         this.props.fetchWeather(this.state.term);
         this.setState({ term: '' });
-        
+
     }
-    
+
     render(){
         return (
             <form onSubmit={this.onFormSubmit} className ="input-group">
@@ -40,7 +40,7 @@ class SearchBar extends Component {
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators( {fetchWeather}, dispatch);
+    return bindActionCreators( { fetchWeather }, dispatch);
 }
 
 export default connect(null, mapDispatchToProps)(SearchBar);
