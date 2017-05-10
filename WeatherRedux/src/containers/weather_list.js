7b30df7a200c5e8b8005c340c50255651d91921c
Leland Krych <Leland.Krych@gmail.com>
2017-05-09 20:55:31 -0700

    Add units to sparkliens charts

--- WeatherRedux/src/containers/weather_list.js
+++ WeatherRedux/src/containers/weather_list.js
@@ -6,16 +6,16 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map( weather => weather.main.temp);
-    const humidity = cityData.list.map( weather => weather.main.humidity);
-    const pressure = cityData.list.map( weather => weather.main.pressure);
+    const humidities = cityData.list.map( weather => weather.main.humidity);
+    const pressures = cityData.list.map( weather => weather.main.pressure);
 
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        <td> <Chart data={temps} color="orange" units="K" /></td>
+        <td> <Chart data={pressures} color="blue" units="hPa" /></td>
+        <td> <Chart data={humidities} color="green" units="%" /></td>
       </tr>
     );
   }
@@ -26,9 +26,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
